6ccbdfb970505a5d997ac643460fc61f252fa76c
peterlxb <peterlxb@gmail.com>
2018-06-01 07:39:24 +0800

    Add components

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <h1>Welcome to React</h1>
       </div>
     );
   }
