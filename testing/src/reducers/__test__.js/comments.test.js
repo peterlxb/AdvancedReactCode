5fd30436b5b9329ff172ee4591b34c4f810d026b
peterlxb <peterlxb@gmail.com>
2018-06-10 19:53:27 +0800

    Testing reducers

---
+++ testing/src/reducers/__test__.js/comments.test.js
@@ -0,0 +1,13 @@
+import commentReducer from "reducers/comments";
+import { SAVE_COMMENT } from "actions/types";
+
+it("handles actions of type SAVE_COMMENT", () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: "New comment"
+  };
+
+  const newState = commentReducer([], action);
+  //console.log(newState);
+  expect(newState).toEqual(["New comment"]);
+});
