76bb76dd7d82eb99d5f6c2320082f2a66b64fc54
peterlxb <peterlxb@gmail.com>
2018-06-11 21:36:00 +0800

    Add redux to commentList

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const CommentList = () => {
-  return (
-    <div>
-      <h1>Comment List</h1>
-    </div>
-  );
-};
+class CommentList extends Component {
+  render() {
+    const { comments } = this.props;
+    let commentList;
 
-export default CommentList;
+    if (comments.length != 0) {
+      commentList = comments.map((comment, index) => (
+        <li key={index}>{comment}</li>
+      ));
+    } else {
+      commentList = <h4>Loading Comments</h4>;
+    }
+
+    return <div>{commentList}</div>;
+  }
+}
+
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+export default connect(mapStateToProps)(CommentList);
