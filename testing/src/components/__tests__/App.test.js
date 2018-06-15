42c43cf9de98e90cbe30fcf72f2453f5865da7b0
peterlxb <peterlxb@gmail.com>
2018-06-15 10:09:12 +0800

    modified App.test.js

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
-import App from "components//App";
+import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
